a5084e312b703c7e538d07f71ba1141cfae891a8
hades948 <hades948@gmail.com>
2020-02-23 01:20:20 -0500

    End of day 1

--- Hackathon/src/main/java/com/theballotbears/hackathon/GameScreen.java
+++ Hackathon/src/main/java/com/theballotbears/hackathon/GameScreen.java
@@ -1,8 +1,10 @@
 package com.theballotbears.hackathon;
 
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
+import com.tylerroyer.molasses.Config;
 import com.tylerroyer.molasses.Game;
 import com.tylerroyer.molasses.GameGraphics;
 import com.tylerroyer.molasses.Screen;
@@ -14,6 +16,11 @@ import org.apache.commons.lang3.mutable.MutableBoolean;
 import org.apache.commons.lang3.mutable.MutableInt;
 
 public class GameScreen extends Screen {
+    private boolean scissors = false;
+    private boolean key = false;
+    private boolean rope = false;
+    private boolean earPiece = false;
+    private String info = "";
     private TileMap tileMap;
     private Player player;
     private final int START_X = 7, START_Y = 6;
@@ -24,7 +31,7 @@ public class GameScreen extends Screen {
     private MutableBoolean playerCanMoveRight = new MutableBoolean(false);
     private MutableBoolean playerCanMoveDown = new MutableBoolean(false);
     private MutableBoolean playerCanMoveLeft = new MutableBoolean(false);
-    private MutableBoolean canInspect = new MutableBoolean(false);
+    private MutableBoolean doInteract = new MutableBoolean(false);
 
     @Override
     public void onFocus() {
@@ -45,14 +52,12 @@ public class GameScreen extends Screen {
         Event playerDirectionDown = new SetIntegerEvent(playerDirection, 2);
         Event playerDirectionLeft = new SetIntegerEvent(playerDirection, 3);
 
-        Event inspectEvent = new LoggerEvent("Inspect.");
+        Event interactEvent = new ToggleOnEvent(doInteract);
 
         playerYUp.addCondition(playerCanMoveUp);
         playerXUp.addCondition(playerCanMoveRight);
         playerYDown.addCondition(playerCanMoveDown);
         playerXDown.addCondition(playerCanMoveLeft);
-
-        inspectEvent.addCondition(canInspect);
 
         Game.getKeyboardHandler().addEvent(KeyEvent.VK_UP, playerDirectionUp);
         Game.getKeyboardHandler().addEvent(KeyEvent.VK_UP, playerYUp);
@@ -66,13 +71,26 @@ public class GameScreen extends Screen {
         Game.getKeyboardHandler().addEvent(KeyEvent.VK_LEFT, playerDirectionLeft);
         Game.getKeyboardHandler().addEvent(KeyEvent.VK_LEFT, playerXDown);
 
-        Game.getKeyboardHandler().addEvent(KeyEvent.VK_I, inspectEvent);
+        Game.getKeyboardHandler().addEvent(KeyEvent.VK_SPACE, interactEvent);
+
+        Game.getAudioHandler().loadMusic("ride.wav");
+        Game.getAudioHandler().loadSoundEffect("cut.wav");
+        Game.getAudioHandler().loadSoundEffect("key_in_door.wav");
+        Game.getAudioHandler().loadSoundEffect("chest.wav");
+        Game.getAudioHandler().loadSoundEffect("lasso.wav");
+        Game.getAudioHandler().loadSoundEffect("end.wav");
+
+        Game.getAudioHandler().playMusic("ride.wav");
     }
 
     @Override
     public void render(GameGraphics g) {
         tileMap.render(g);
         player.render(g);
+
+        g.setColor(Color.WHITE);
+        g.setFont(Config.gameFont.deriveFont(12.0f));
+        g.drawString(info, 5, 20);
     }
 
     @Override
@@ -91,7 +109,68 @@ public class GameScreen extends Screen {
         playerCanMoveRight.setValue(tileRight == null ? false : !tileRight.isSolid());
         playerCanMoveDown.setValue(tileDown == null ? false : !tileDown.isSolid());
         playerCanMoveLeft.setValue(tileLeft == null ? false : !tileLeft.isSolid());
-        canInspect.setValue(tileInFront == null ? false : tileInFront.getFlipBookName().equals("object.mfb"));//todo
+
+        if (doInteract.isTrue()) {
+            if (tileInFront != null) {
+                String tileInFrontName = tileInFront.getFlipBookName();
+
+                if (tileInFrontName.equals("16.mfb")) { // Guard
+                    if (!scissors) {
+                        info = "The guard's asleep. Better not wake him. He has a key around his neck, though.";
+                    } else if (!key) {
+                        info = "You snip the cord holding the key. Bingo! Now to get out of here";
+                        key = true;
+                        Game.getAudioHandler().playSoundEffect("cut.wav");
+                    } else {
+                        info = "He's still sleeping. I better leave before he wakes up!";
+                    }
+                } else if (tileInFrontName.equals("22.mfb")) { // Door
+                    if (!key) {
+                        info = "They're keeping my earpiece back there, but the door is locked.";
+                    } else {
+                        info = "You put the key in the lock. The door quietly swings open.";
+                        tileMap.setTile(11, 1, "23.mfb");
+                        tileMap.getTile(11, 1).setIsSolid(false);
+                        tileMap.setTile(11, 0, "2.mfb");
+                        tileMap.getTile(11, 0).setIsSolid(true);
+                        Game.getAudioHandler().playSoundEffect("key_in_door.wav");
+                    }
+                } else if (tileInFrontName.equals("1.mfb")) { // Pipe
+                    if (!rope) {
+                        info = "You manage to fray the rope on the rusty pipe. Eureka! Your hands are free.";
+                        rope = true;
+                        Game.getAudioHandler().playSoundEffect("cut.wav");
+                    } else {
+                        info = "No use for this anymore. Hope I didn't get tetnus.";
+                    }
+                } else if (tileInFrontName.equals("27.mfb")) { // Normal crates
+                    info = "Just a bunch of useless crates.  Nothing seems useful.";
+                } else if (tileInFrontName.equals("28.mfb")) { // Special crate ;)
+                    if (!rope) {
+                        info = "You peer behind the crates. There’s scissors on the table. If only your hands were free.";
+                    } else {
+                        info = "You tie the rope into a lasso to snag the scissors. These would've been handy earlier...";
+                        scissors = true;
+                        tileMap.setTile(1, 7, "11B.mfb");
+                        Game.getAudioHandler().playSoundEffect("lasso.wav");
+                    }
+                } else if (tileInFrontName.equals("6.mfb") || tileInFrontName.equals("9.mfb")) { // Chest
+                    info = "There's a note in the box. \"To do: Fix sharp rusty pipe. Someone could get cut!\"";
+                    Game.getAudioHandler().playSoundEffect("chest.wav");
+                } else if (tileInFrontName.equals("29.mfb")) { // Junk
+                    info = "There's junk everywhere!";
+                } else if (tileInFrontName.equals("2.mfb")) { // Earpiece
+                    earPiece = true;
+                    info = "You contact the U.S. and save the day!";
+                    Game.getAudioHandler().playSoundEffect("end.wav");
+                    Game.getAudioHandler().pauseMusic();
+                } else {
+                    info = "There's nothing here.";
+                }
+            }
+
+            doInteract.setFalse();
+        }
 
         tileMap.update();
         player.update();
